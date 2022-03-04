783a4ef160b8cb7ad97fcced85bbad2d8dc31df4
abiona01 <quadratwemimo@gmail.com>
2022-03-04 16:02:16 +0100

    update

--- app/Models/Disease.php
+++ app/Models/Disease.php
@@ -4,8 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Medicine;
 
 class Disease extends Model
 {
     use HasFactory;
+    public function medicines () {
+        return $this->belongsToMany(Medicine::class, 'medicine_reactions');
+    }
 }
--- app/Models/Medicine.php
+++ app/Models/Medicine.php
@@ -4,9 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Disease;
 
 class Medicine extends Model
 {
+   
     use HasFactory;
-    $protected $casts = ['contraindications'=>'array'];
+    protected $casts = ['contraindications'=>'array'];
+
+    public function diseases () {
+        return $this->belongsToMany(Disease::class, 'medicine_reactions');
+    }
 }
