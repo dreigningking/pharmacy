adc58772c2d176d24988de9c871bb11883612493
abiona01 <quadratwemimo@gmail.com>
2022-03-09 16:54:47 +0100

    medical history update

--- resources/views/main/pharmacy/assessment/add.blade.php
+++ resources/views/main/pharmacy/assessment/add.blade.php
@@ -109,9 +109,9 @@
                                     <button type="button" class="btn btn-text" data-toggle="collapse"
                                         data-target="#physical_assessment">Patient's Physical Assessment</button>
 
-                                    <div class="row w-100 collapse show">
-                                        <div class="row mt-4 pl-4 pr-2  w-100 justify-content-center physical-assessment"
-                                            id="physical_assessment">
+                                    <div class="row w-100 collapse show" id="physical_assessment">
+                                        <div
+                                            class="row mt-4 pl-4 pr-2  w-100 justify-content-center physical-assessment">
 
                                             <div class="col-11 physical d-flex">
                                                 <div class="col-md-6 pl-0 pr-0">
@@ -141,7 +141,7 @@
 
 
                                         </div>
-                                        <div class="add-more pl-4">
+                                        <div class="plus-more add-more">
                                             <a href="javascript:void(0);" class="add-physical"><i
                                                     class="fa fa-plus-circle"></i> Add More</a>
                                         </div>
@@ -151,7 +151,107 @@
                                 <div class="row justify-content-start mt-4 w-100 ">
                                     <button type="button" class="btn btn-text" data-toggle="collapse"
                                         data-target="#medical_history">Medical History</button>
+                                    <div class="row collapse show w-100" id="medical_history">
+                                        <div class="row w-100">
+                                            <div class="row mt-4 pl-4 pr-2  w-100 justify-content-center current-med">
+                                                <h4 class="text-muted align-text-start mb-2">Medications for Current
+                                                    Medical
+                                                    Contdition</h4>
+                                                <div
+                                                    class="col-11 physical d-flex align-items-center mt-2 mb-2 current">
+                                                    <div class="col-md-7 pl-0 pr-0">
+                                                        <div class="form-group row">
 
+                                                            <div class="col-lg-11">
+                                                                <div class="form-group d-flex align-items-center">
+                                                                    <label for="usr">Medication:</label>
+                                                                    <input type="text" name="" id=""
+                                                                        class="ml-2 form-control">
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                    <div class="col-md-4 pr-0 pl-0">
+                                                        <div class="form-group row">
+                                                            <div class="col-lg-4">
+                                                                <p>Effective?</p>
+                                                            </div>
+                                                            <div class="col-lg-8">
+                                                                <div class="form-check-inline">
+                                                                    <label class="form-check-label">
+                                                                        <input type="radio" class="form-check-input"
+                                                                            name="optradio">Yes
+                                                                    </label>
+                                                                </div>
+                                                                <div class="form-check-inline">
+                                                                    <label class="form-check-label">
+                                                                        <input type="radio" class="form-check-input"
+                                                                            name="optradio">No
+                                                                    </label>
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+
+
+                                            </div>
+                                            <div class="plus-more add-more">
+                                                <a href="javascript:void(0);" class="add-current"><i
+                                                        class="fa fa-plus-circle"></i> Add More</a>
+                                            </div>
+                                        </div>
+                                        <div class="row w-100 mt-4">
+                                            <div class="row mt-4 pl-4 pr-2  w-100 justify-content-center past-med">
+                                                <h4 class="text-muted align-text-start mb-2">Medications for Past
+                                                    Medical
+                                                    Contdition</h4>
+                                                <div class="col-11 physical d-flex align-items-center mt-2 mb-2 past">
+                                                    <div class="col-md-7 pl-0 pr-0">
+                                                        <div class="form-group row">
+
+                                                            <div class="col-lg-11">
+                                                                <div class="form-group d-flex align-items-center">
+                                                                    <label for="usr">Medication:</label>
+                                                                    <input type="text" name="" id=""
+                                                                        class="ml-2 form-control">
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                    <div class="col-md-4 pr-0 pl-0">
+                                                        <div class="form-group row">
+                                                            <div class="col-lg-4">
+                                                                <p>Effective?</p>
+                                                            </div>
+                                                            <div class="col-lg-8">
+                                                                <div class="form-check-inline">
+                                                                    <label class="form-check-label">
+                                                                        <input type="radio" class="form-check-input"
+                                                                            name="optradio">Yes
+                                                                    </label>
+                                                                </div>
+                                                                <div class="form-check-inline">
+                                                                    <label class="form-check-label">
+                                                                        <input type="radio" class="form-check-input"
+                                                                            name="optradio">No
+                                                                    </label>
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+
+
+                                            </div>
+
+                                            <div class="plus-more add-more">
+                                                <a href="javascript:void(0);" class="add-past"><i
+                                                        class="fa fa-plus-circle"></i> Add More</a>
+                                            </div>
+
+                                        </div>
+                                    </div>
                                 </div>
                             </form>
                         </div>
@@ -206,6 +306,100 @@ $(".add-physical").on('click', function() {
         '</div>';
 
     $(".physical-assessment").append(regcontent);
+    return false;
+});
+
+$(".current-med").on('click', '.trash', function() {
+    $(this).closest('.current').remove();
+    return false;
+});
+$(".add-current").on('click', function() {
+    console.log("meh")
+    var regcontent = ' <div class = "col-11 physical d-flex align-items-start mt-2 mb-2 current" > ' +
+        ' <div class="col-md-7 pl-0 pr-0">' +
+        '<div class="form-group row">' +
+        ' <div class="col-lg-11">' +
+        '<div class="form-group d-flex align-items-center">' +
+        '<label for="usr">Medication:</label>' +
+        '<input type="text" name="" id="" class = "ml-2 form-control" > ' +
+        '</div>' +
+        '</div>' +
+        '</div>' +
+        '</div>' +
+        ' <div class="col-md-4 pr-0 pl-0">' +
+        ' <div class="form-group row">' +
+        '<div class="col-lg-4">' +
+        '<p>Effective?</p>' +
+        '</div>' +
+        '<div class="col-lg-8">' +
+        '<div class="form-check-inline">' +
+        '<label class="form-check-label">' +
+        '<input type="radio" class="form-check-input" name = "optradio" > Yes ' +
+        '</label>' +
+        '</div>' +
+        '<div class="form-check-inline">' +
+        '<label class="form-check-label">' +
+        '<input type="radio" class="form-check-input" name = "optradio" > No ' +
+        '</label>' +
+        '</div>' +
+        '</div>' +
+        '</div>' +
+        '</div>' +
+        '<div class="col-1 pr-0 pl-0 d-flex align-items-start justify-content-end" >' +
+        '<label class="d-md-block d-sm-none d-none">&nbsp;</label>' +
+        '<a href="#" class="btn btn-danger trash"><i class="far fa-trash-alt"></i></a>' +
+        '</div>' +
+        '</div>' +
+        '</div>';
+
+    $(".current-med").append(regcontent);
+    return false;
+});
+
+$(".past-med").on('click', '.trash', function() {
+    $(this).closest('.past').remove();
+    return false;
+});
+$(".add-past").on('click', function() {
+    console.log("meh")
+    var regcontent = ' <div class = "col-11 physical d-flex align-items-start mt-2 mb-2 past" > ' +
+        ' <div class="col-md-7 pl-0 pr-0">' +
+        '<div class="form-group row">' +
+        ' <div class="col-lg-11">' +
+        '<div class="form-group d-flex align-items-center">' +
+        '<label for="usr">Medication:</label>' +
+        '<input type="text" name="" id="" class = "ml-2 form-control" > ' +
+        '</div>' +
+        '</div>' +
+        '</div>' +
+        '</div>' +
+        ' <div class="col-md-4 pr-0 pl-0">' +
+        ' <div class="form-group row">' +
+        '<div class="col-lg-4">' +
+        '<p>Effective?</p>' +
+        '</div>' +
+        '<div class="col-lg-8">' +
+        '<div class="form-check-inline">' +
+        '<label class="form-check-label">' +
+        '<input type="radio" class="form-check-input" name = "optradio" > Yes ' +
+        '</label>' +
+        '</div>' +
+        '<div class="form-check-inline">' +
+        '<label class="form-check-label">' +
+        '<input type="radio" class="form-check-input" name = "optradio" > No ' +
+        '</label>' +
+        '</div>' +
+        '</div>' +
+        '</div>' +
+        '</div>' +
+        '<div class="col-1 pr-0 pl-0 d-flex align-items-start justify-content-end" >' +
+        '<label class="d-md-block d-sm-none d-none">&nbsp;</label>' +
+        '<a href="#" class="btn btn-danger trash"><i class="far fa-trash-alt"></i></a>' +
+        '</div>' +
+        '</div>' +
+        '</div>';
+
+    $(".past-med").append(regcontent);
     return false;
 });
 </script>
