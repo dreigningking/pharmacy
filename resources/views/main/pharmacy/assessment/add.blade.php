066ccce9d40d77b503a332a09a0e2f739a27338f
abiona01 <quadratwemimo@gmail.com>
2022-03-11 02:21:25 -0800

    update

--- resources/views/main/pharmacy/assessment/add.blade.php
+++ resources/views/main/pharmacy/assessment/add.blade.php
@@ -33,7 +33,7 @@
                 <!-- Basic Information -->
                 <div class="card">
                     <div class="card-body">
-                        <h4 class="card-title">Add Assessment</h4>
+                        <h4 class="card-title assessment-title">ADD ASSESSMENT</h4>
                         <div class="row justify-content-center">
                             <form class="search-form d-flex">
                                 <input class="search" type="text"
@@ -47,7 +47,7 @@
                             <form action="" class="w-100">
                                 <div class="row justify-content-start mt-4 w-100">
 
-                                    <button type="button" class="btn btn-text" data-toggle="collapse"
+                                    <button type="button" class="btn btn-text open" data-toggle="collapse"
                                         data-target="#personal_info">Patient's Personal Information</button>
 
                                     <div class="row mt-4 pl-4 pr-2 collapse show w-100 justify-content-center"
@@ -106,7 +106,7 @@
                                 </div>
                                 <div class="row justify-content-start mt-4 w-100 ">
 
-                                    <button type="button" class="btn btn-text" data-toggle="collapse"
+                                    <button type="button" class="btn btn-text open" data-toggle="collapse"
                                         data-target="#physical_assessment">Patient's Physical Assessment</button>
 
                                     <div class="row w-100 collapse show" id="physical_assessment">
@@ -149,58 +149,10 @@
 
                                 </div>
                                 <div class="row justify-content-start mt-4 w-100 ">
-                                    <button type="button" class="btn btn-text" data-toggle="collapse"
+                                    <button type="button" class="btn btn-text open" data-toggle="collapse"
                                         data-target="#medical_history">Medical History</button>
                                     <div class="row collapse show w-100" id="medical_history">
-                                        <div class="row w-100">
-                                            <div class="row mt-4 pl-4 pr-2  w-100 justify-content-center current-med">
-                                                <h4 class="text-muted align-text-start mb-2">Medications for Current
-                                                    Medical
-                                                    Contdition</h4>
-                                                <div
-                                                    class="col-11 physical d-flex align-items-center mt-2 mb-2 current">
-                                                    <div class="col-md-7 pl-0 pr-0">
-                                                        <div class="form-group row">
 
-                                                            <div class="col-lg-11">
-                                                                <div class="form-group d-flex align-items-center">
-                                                                    <label for="usr">Medication:</label>
-                                                                    <input type="text" name="" id=""
-                                                                        class="ml-2 form-control">
-                                                                </div>
-                                                            </div>
-                                                        </div>
-                                                    </div>
-                                                    <div class="col-md-4 pr-0 pl-0">
-                                                        <div class="form-group row">
-                                                            <div class="col-lg-4">
-                                                                <p>Effective?</p>
-                                                            </div>
-                                                            <div class="col-lg-8">
-                                                                <div class="form-check-inline">
-                                                                    <label class="form-check-label">
-                                                                        <input type="radio" class="form-check-input"
-                                                                            name="optradio">Yes
-                                                                    </label>
-                                                                </div>
-                                                                <div class="form-check-inline">
-                                                                    <label class="form-check-label">
-                                                                        <input type="radio" class="form-check-input"
-                                                                            name="optradio">No
-                                                                    </label>
-                                                                </div>
-                                                            </div>
-                                                        </div>
-                                                    </div>
-                                                </div>
-
-
-                                            </div>
-                                            <div class="plus-more add-more">
-                                                <a href="javascript:void(0);" class="add-current"><i
-                                                        class="fa fa-plus-circle"></i> Add More</a>
-                                            </div>
-                                        </div>
                                         <div class="row w-100 mt-4">
                                             <div class="row mt-4 pl-4 pr-2  w-100 justify-content-center past-med">
                                                 <h4 class="text-muted align-text-start mb-2">Medications for Past
@@ -253,17 +205,132 @@
                                         </div>
                                     </div>
                                 </div>
+                                <div class="row justify-content-start mt-4 w-100">
+
+                                    <button type="button" class="btn btn-text open" data-toggle="collapse"
+                                        data-target="#lab">Laboratory Findings</button>
+
+                                    <div class="row mt-4 pl-4 pr-2 collapse show w-100 justify-content-center" id="lab">
+
+                                        <div class="row w-100">
+                                            <div class="col-md-12">
+                                                <div class="form-group row">
+                                                    <label class="col-lg-2 col-form-label pr-0">Laboratory
+                                                        Result</label>
+                                                    <div class="col-lg-10">
+                                                        <textarea class="form-control" rows="4"
+                                                            id="laboratory_result"></textarea>
+                                                    </div>
+                                                </div>
+                                            </div>
+
+                                        </div>
+
+
+                                    </div>
+                                </div>
+                                <div class="row justify-content-start mt-4 w-100">
+
+                                    <button type="button" class="btn btn-text open" data-toggle="collapse"
+                                        data-target="#diagnosis">Diagnosis</button>
+
+                                    <div class="row mt-4 pl-4 pr-2 collapse show w-100 justify-content-center"
+                                        id="diagnosis">
+
+                                        <div class="row w-100">
+                                            <div class="col-md-12">
+                                                <div class="form-group row">
+                                                    <label class="col-lg-2 col-form-label pr-0">Diagnosis</label>
+                                                    <div class="col-lg-10">
+                                                        <textarea class="form-control" rows="2"
+                                                            id="laboratory_result"></textarea>
+                                                    </div>
+                                                </div>
+                                            </div>
+
+                                        </div>
+
+
+                                    </div>
+                                </div>
+                                <div class="row justify-content-start mt-4 w-100">
+
+                                    <button type="button" class="btn btn-text open" data-toggle="collapse"
+                                        data-target="#prescriptions">Prescription</button>
+
+                                    <div class="row mt-4  pr-2 collapse show w-100 justify-content-center"
+                                        id="prescriptions">
+
+                                        <div class="row w-100 pl-4">
+                                            <div class="col-md-12 w-100 prescriptions">
+                                                <div class="prescription">
+                                                    <div class="form-group row">
+                                                        <label class="col-lg-2 col-form-label pr-0">Prescription</label>
+                                                        <div class="col-lg-10">
+                                                            <input type="text" class="form-control">
+                                                        </div>
+                                                    </div>
+                                                </div>
+
+
+
+                                            </div>
+                                            <div class="plus-more add-more">
+                                                <a href="javascript:void(0);" class="add-prescription"><i
+                                                        class="fa fa-plus-circle"></i> Add More</a>
+                                            </div>
+                                        </div>
+
+
+                                    </div>
+                                </div>
+                                <div class="row justify-content-start mt-4 w-100">
+
+                                    <button type="button" class="btn btn-text open" data-toggle="collapse"
+                                        data-target="#plan">Non-Medication Plan</button>
+
+                                    <div class="row mt-4  pr-2 collapse show w-100 justify-content-center" id="plan">
+
+                                        <div class="row w-100 pl-4">
+                                            <div class="col-md-12 w-100 plans">
+                                                <div class="plann">
+                                                    <div class="form-group row">
+                                                        <label class="col-lg-2 col-form-label pr-0">Plan</label>
+                                                        <div class="col-lg-10">
+                                                            <input type="text" class="form-control">
+                                                        </div>
+                                                    </div>
+                                                </div>
+
+
+
+                                            </div>
+                                            <div class="plus-more add-more">
+                                                <a href="javascript:void(0);" class="add-plan"><i
+                                                        class="fa fa-plus-circle"></i> Add More</a>
+                                            </div>
+                                        </div>
+
+
+                                    </div>
+                                </div>
+                                <div class="row pr-4 mt-4 justify-content-end">
+                                    <button type="submit" class="btn btn-primary pl-2 pr-2">Save</button>
+                                </div>
                             </form>
                         </div>
-
-
                     </div>
+
                 </div>
+
 
             </div>
         </div>
 
     </div>
+</div>
+
+</div>
 
 </div>
 <!-- /Page Content -->
@@ -309,52 +376,6 @@ $(".add-physical").on('click', function() {
     return false;
 });
 
-$(".current-med").on('click', '.trash', function() {
-    $(this).closest('.current').remove();
-    return false;
-});
-$(".add-current").on('click', function() {
-    console.log("meh")
-    var regcontent = ' <div class = "col-11 physical d-flex align-items-start mt-2 mb-2 current" > ' +
-        ' <div class="col-md-7 pl-0 pr-0">' +
-        '<div class="form-group row">' +
-        ' <div class="col-lg-11">' +
-        '<div class="form-group d-flex align-items-center">' +
-        '<label for="usr">Medication:</label>' +
-        '<input type="text" name="" id="" class = "ml-2 form-control" > ' +
-        '</div>' +
-        '</div>' +
-        '</div>' +
-        '</div>' +
-        ' <div class="col-md-4 pr-0 pl-0">' +
-        ' <div class="form-group row">' +
-        '<div class="col-lg-4">' +
-        '<p>Effective?</p>' +
-        '</div>' +
-        '<div class="col-lg-8">' +
-        '<div class="form-check-inline">' +
-        '<label class="form-check-label">' +
-        '<input type="radio" class="form-check-input" name = "optradio" > Yes ' +
-        '</label>' +
-        '</div>' +
-        '<div class="form-check-inline">' +
-        '<label class="form-check-label">' +
-        '<input type="radio" class="form-check-input" name = "optradio" > No ' +
-        '</label>' +
-        '</div>' +
-        '</div>' +
-        '</div>' +
-        '</div>' +
-        '<div class="col-1 pr-0 pl-0 d-flex align-items-start justify-content-end" >' +
-        '<label class="d-md-block d-sm-none d-none">&nbsp;</label>' +
-        '<a href="#" class="btn btn-danger trash"><i class="far fa-trash-alt"></i></a>' +
-        '</div>' +
-        '</div>' +
-        '</div>';
-
-    $(".current-med").append(regcontent);
-    return false;
-});
 
 $(".past-med").on('click', '.trash', function() {
     $(this).closest('.past').remove();
@@ -400,6 +421,54 @@ $(".add-past").on('click', function() {
         '</div>';
 
     $(".past-med").append(regcontent);
+    return false;
+});
+
+
+$(".prescriptions").on('click', '.trash', function() {
+    $(this).closest('.prescription').remove();
+    return false;
+});
+$(".add-prescription").on('click', function() {
+    console.log("meh")
+    var regcontent = '<div class="col-md-12 pl-0 prescription">' +
+        ' <div class="form-group row">' +
+        '<label class="col-lg-2 col-form-label pr-0">Prescription</label>' +
+        '<div class="col-lg-9">' +
+        '<input type="text" class="form-control">' +
+        '</div>' +
+        '<div class="col-1 pr-0 pl-0 d-flex align-items-start justify-content-end" >' +
+        '<label class="d-md-block d-sm-none d-none">&nbsp;</label>' +
+        '<a href="#" class="btn btn-danger trash"><i class="far fa-trash-alt"></i></a>' +
+        '</div>' +
+        '</div>' +
+
+        '</div>';
+
+    $(".prescriptions").append(regcontent);
+    return false;
+});
+$(".plans").on('click', '.trash', function() {
+    $(this).closest('.plan').remove();
+    return false;
+});
+$(".add-plan").on('click', function() {
+    console.log("meh")
+    var regcontent = '<div class="col-md-12 pl-0 plan">' +
+        ' <div class="form-group row">' +
+        '<label class="col-lg-2 col-form-label pr-0"></label>' +
+        '<div class="col-lg-9">' +
+        '<input type="text" class="form-control">' +
+        '</div>' +
+        '<div class="col-1 pr-0 pl-0 d-flex align-items-start justify-content-end" >' +
+        '<label class="d-md-block d-sm-none d-none">&nbsp;</label>' +
+        '<a href="#" class="btn btn-danger trash"><i class="far fa-trash-alt"></i></a>' +
+        '</div>' +
+        '</div>' +
+
+        '</div>';
+
+    $(".plans").append(regcontent);
     return false;
 });
 </script>
