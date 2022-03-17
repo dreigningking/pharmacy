1b63d8fd432f86fc501c264a633e3769e0fa0fe6
abiona01 <quadratwemimo@gmail.com>
2022-03-17 17:19:16 +0100

    update

--- resources/views/main/pharmacy/supply.blade.php
+++ resources/views/main/pharmacy/supply.blade.php
@@ -34,61 +34,95 @@
 
                 <!-- Page Wrapper -->
                 <div class="page-wrapper">
-                   
-                        <div class="card">
-                            <div class="card-body">
 
-                                <form action="">
-                                    <div class="container-fluid pl-0 pr-0">
-                                        <div class="row ">
-                                            <div class="col-md-7">
-                                                <div class="form-group row align-items-center">
-                                                    <label for="usr" class="col-3">Invoice No:</label>
-                                                    <input type="text" class="form-control col-9" id="usr">
-                                                </div>
-                                            </div>
-                                            <div class="col-md-5">
-                                                <div class="row w-100 justify-content-end">
-                                                    <a class="btn btn-primary btn-lg" data-toggle="modal" href="#">Add
-                                                        New
-                                                        Drug</a>
-                                                </div>
+                    <div class="card">
+                        <h4 class="card-header mb-4">New Supply</h4>
+                        <div class="card-body">
+
+                            <form action="">
+                                <div class="container-fluid pl-0 pr-0">
+                                    <div class="row ">
+                                        <div class="col-md-7">
+                                            <div class="form-group row align-items-center">
+                                                <label for="usr" class="col-3">Invoice No:</label>
+                                                <input type="text" class="form-control col-9" id="usr">
                                             </div>
                                         </div>
-                                        <div class="row ">
-                                            <div class="col-md-7">
-                                                <div class="form-group row align-items-center">
-                                                    <label for="usr" class="col-3">Supply:</label>
-                                                    <select class="form-control col-9" id="sel1">
-                                                        <option>1</option>
-                                                        <option>2</option>
-                                                        <option>3</option>
-                                                        <option>4</option>
-                                                    </select>
-                                                </div>
+                                        <div class="col-md-5">
+                                            <div class="row w-100 justify-content-end">
+                                               <div class="col-md-6">
+                                               <a class="btn btn-primary btn-lg w-100" data-toggle="modal" href="#">Add
+                                                    New
+                                                    Drug</a>
+                                               </div>
                                             </div>
-                                            <div class="col-md-5">
-                                                <div class="row w-100 justify-content-end">
-                                                    <a class="btn btn-primary btn-lg" data-toggle="modal" href="#">Add
-                                                        Supplier</a>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <div class="row ">
-                                            <div class="col-md-7">
-                                                <div class="form-group row align-items-center">
-                                                    <label for="usr" class="col-3">Date:</label>
-                                                    <input type="date" class="form-control col-9" id="usr">
-                                                </div>
-                                            </div>
-
                                         </div>
                                     </div>
-                                </form>
-                            </div>
+                                    <div class="row ">
+                                        <div class="col-md-7">
+                                            <div class="form-group row align-items-center">
+                                                <label for="usr" class="col-3">Supply:</label>
+                                                <select class="form-control col-9" id="sel1">
+                                                    <option>1</option>
+                                                    <option>2</option>
+                                                    <option>3</option>
+                                                    <option>4</option>
+                                                </select>
+                                            </div>
+                                        </div>
+                                        <div class="col-md-5">
+                                            <div class="row w-100 justify-content-end">
+                                               <div class="col-md-6"> <a class="btn btn-primary btn-lg w-100" data-toggle="modal" href="#">Add
+                                                    Supplier</a></div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="row ">
+                                        <div class="col-md-7">
+                                            <div class="form-group row align-items-center">
+                                                <label for="usr" class="col-3">Date:</label>
+                                                <input type="date" class="form-control col-9" id="usr">
+                                            </div>
+                                        </div>
 
+                                    </div>
+                                    <div class="row w-100 mt-4">
+                                        <div class="supplies row w-100">
+                                            <div class="col-11 supply d-flex">
+                                                <div class="col-md-7">
+                                                    <div class="form-group row align-items-center">
+                                                        <label for="usr" class="col-3">Supply:</label>
+                                                        <select class="form-control col-9" id="sel1">
+                                                            <option>1</option>
+                                                            <option>2</option>
+                                                            <option>3</option>
+                                                            <option>4</option>
+                                                        </select>
+                                                    </div>
+                                                </div>
+                                                <div class="col-md-5">
+                                                    <div class="form-group row align-items-center">
+                                                        <label for="usr" class="col-3">Quantity:</label>
+                                                        <input type="number" class="form-control col-9" id="usr">
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="plus-more add-more">
+                                            <a href="javascript:void(0);" class="add-supply"><i
+                                                    class="fa fa-plus-circle"></i> Add More</a>
+                                        </div>
+                                    </div>
+                                    <div class="row mt-4 ml-2">
+                                        <button class="btn btn-primary mt-4">Save as Draft</button>
+                                        <button class="btn btn-primary ml-4 mt-4">Save and Send</button>
+                                    </div>
+                                </div>
+                            </form>
                         </div>
-                    
+
+                    </div>
+
                 </div>
             </div>
             <!-- /Page Wrapper -->
@@ -103,3 +137,46 @@
 <!-- /Page Content -->
 
 @endsection
+
+@push('scripts')
+<script>
+$(".supplies").on('click', '.trash', function() {
+    $(this).closest('.supply').remove();
+    return false;
+});
+$(".add-supply").on('click', function() {
+
+    var regcontent = '<div class="supply row w-100 pl-4">' +
+
+        '<div class="col-11 pr-0 pl-0 d-flex">' +
+
+        '<div class="col-md-7">' +
+        '<div class="form-group row align-items-center">' +
+        '<label for="usr" class="col-3">Supply:</label>' +
+        '<select class="form-control col-9" id="sel1">' +
+        '<option>1</option>' +
+        '<option>2</option>' +
+        '<option>3</option>' +
+        '<option>4</option>' +
+        '</select>' +
+        '</div>' +
+        '</div>' +
+
+        ' <div class="col-md-5">' +
+        '<div class="form-group row align-items-center">' +
+        '<label for="usr" class="col-3">Quantity:</label>' +
+        '<input type="number" class="form-control col-9" id="usr">' +
+        '</div>' +
+        '</div>' +
+
+        '<div class="col-1 pr-0 pl-0 d-flex align-items-start justify-content-end" >' +
+        '<label class="d-md-block d-sm-none d-none">&nbsp;</label>' +
+        '<a href="#" class="btn btn-danger trash"><i class="far fa-trash-alt"></i></a>' +
+        '</div>' +
+        '</div>';
+
+    $(".supplies").append(regcontent);
+    return false;
+});
+</script>
+@endpush
