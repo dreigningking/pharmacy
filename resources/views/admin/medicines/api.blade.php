e82678e7ed75c98e15612de10231ed347b1ec5cb
dreigningking <reigningkingforever@gmail.com>
2022-10-27 14:59:03 +0100

    added modal to api

--- resources/views/admin/medicines/api.blade.php
+++ resources/views/admin/medicines/api.blade.php
@@ -56,12 +56,11 @@
                                     <td class=""> Active </td>
                                     <td class=""> 
                                         <div class="d-flex">
-                                            <a class="btn btn-sm bg-info-light mx-1" > <i class="fe fe-pencil"></i> Edit </a>
-                                            <a class="btn btn-sm bg-primary-light mx-1" > <i class="fe fe-eye"></i> View More </a>
-                                            <a class="btn btn-sm bg-danger-light mx-1" > <i class="fe fe-eye"></i> Delete </a>
+                                            <a class="btn btn-sm bg-info-light mx-1" data-toggle="modal" href="#edit"> <i class="fe fe-pencil"></i> Edit </a>
+                                            <a class="btn btn-sm bg-primary-light mx-1" data-toggle="modal" href="#view"> <i class="fe fe-eye"></i> View More </a>
+                                            <a class="btn btn-sm bg-danger-light mx-1" data-toggle="modal" href="#delete"> <i class="fe fe-eye"></i> Delete </a>
                                         </div>
                                     </td>
-
                                 </tr>
                                 <tr>
                                     <td class="text-center">
@@ -76,9 +75,9 @@
                                     <td class=""> Active </td>
                                     <td class=""> 
                                         <div class="d-flex">
-                                            <a class="btn btn-sm bg-info-light mx-1" > <i class="fe fe-pencil"></i> Edit </a>
-                                            <a class="btn btn-sm bg-primary-light mx-1" > <i class="fe fe-eye"></i> View More </a>
-                                            <a class="btn btn-sm bg-danger-light mx-1" > <i class="fe fe-eye"></i> Delete </a>
+                                            <a class="btn btn-sm bg-info-light mx-1" data-toggle="modal" href="#edit"> <i class="fe fe-pencil"></i> Edit </a>
+                                            <a class="btn btn-sm bg-primary-light mx-1" data-toggle="modal" href="#view"> <i class="fe fe-eye"></i> View More </a>
+                                            <a class="btn btn-sm bg-danger-light mx-1" data-toggle="modal" href="#delete"> <i class="fe fe-eye"></i> Delete </a>
                                         </div>
                                     </td>
 
@@ -91,6 +90,181 @@
         </div>
     </div>
 </div>
+@endsection
+
+@section('modals')
+    <div class="modal fade custom-modal add-modal" id="edit">
+        <div class="modal-dialog modal-dialog-centered">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title">Edit API</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <div class="row">
+                        <div class="col-12">
+                            <form action="#" class="needs-validation" novalidate>
+                                <div class="row my-2">
+                                    <div class="col-md-4">
+                                        <label for="sel1">Name:</label>
+                                    </div>
+                                    <div class="col-md-8">
+                                        <input type="text" class="form-control" name="name" value="Vitamin C" >
+                                    </div>
+                                </div>
+                                <div class="row my-2">
+                                    <div class="col-md-4">
+                                        <label for="sel1">Side Effect:</label>
+                                    </div>
+                                    <div class="col-md-8">
+                                        <textarea class="form-control" name="name" >Fever, Nausea</textarea>
+                                    </div>
+                                </div>        
+                                <div class="row my-2">
+                                    <div class="col-md-4">
+                                        <label for="sel1">Contraindications:</label>
+                                    </div>
+                                    <div class="col-md-8">
+                                        <textarea class="form-control" name="name">Fever, Nausea</textarea>
+                                    </div>
+                                </div>    
+                                <div class="row my-2">
+                                    <div class="col-md-4">
+                                        <label for="sel1">Pregnancy Status:</label>
+                                    </div>
+                                    <div class="col-md-8">
+                                        <input type="text" class="form-control" name="name" value="Not good for pregnant women" >
+                                    </div>
+                                </div> 
+                                <div class="row my-2">
+                                    <div class="col-md-4">
+                                        <label for="sel1">Lactation Status:</label>
+                                    </div>
+                                    <div class="col-md-8">
+                                        <input type="text" class="form-control" name="name" value="No effect on lactating women"    >
+                                    </div>
+                                </div>   
+                                
+
+                                <div class="d-flex my-2 justify-content-between">
+                                    <div class="">
+                                        <a href="#" class="btn btn-success">Update</a>
+                                    </div>
+                                    <div class="">
+                                        <a href="#" class="btn btn-danger">Cancel</a>
+                                    </div>
+                                </div>
+                                
+                                {{-- <button type="submit" class="btn btn-primary pl-4 pr-4 mt-2">Submit</button> --}}
+                            </form>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="modal fade custom-modal add-modal" id="view">
+        <div class="modal-dialog modal-dialog-centered">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title">View API</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <div class="row">
+                        <div class="col-12">
+                            <form action="#" class="needs-validation" novalidate>
+                                <div class="row my-2">
+                                    <div class="col-md-4">
+                                        <label for="sel1">Name:</label>
+                                    </div>
+                                    <div class="col-md-8">
+                                        <input type="text" class="form-control" name="name" value="Vitamin C" readonly>
+                                    </div>
+                                </div>
+                                <div class="row my-2">
+                                    <div class="col-md-4">
+                                        <label for="sel1">Side Effect:</label>
+                                    </div>
+                                    <div class="col-md-8">
+                                        <textarea class="form-control" name="name" readonly>Fever, Nausea</textarea>
+                                    </div>
+                                </div>        
+                                <div class="row my-2">
+                                    <div class="col-md-4">
+                                        <label for="sel1">Contraindications:</label>
+                                    </div>
+                                    <div class="col-md-8">
+                                        <textarea class="form-control" name="name" readonly>Fever, Nausea</textarea>
+                                    </div>
+                                </div>    
+                                <div class="row my-2">
+                                    <div class="col-md-4">
+                                        <label for="sel1">Pregnancy Status:</label>
+                                    </div>
+                                    <div class="col-md-8">
+                                        <input type="text" class="form-control" name="name" value="Not good for pregnant women" readonly>
+                                    </div>
+                                </div> 
+                                <div class="row my-2">
+                                    <div class="col-md-4">
+                                        <label for="sel1">Lactation Status:</label>
+                                    </div>
+                                    <div class="col-md-8">
+                                        <input type="text" class="form-control" name="name" value="No effect on lactating women" readonly>
+                                    </div>
+                                </div>   
+                                
+
+                                <div class="row my-2 justify-content-center">
+                                    <div class="col-md-6">
+                                        <a href="#" class="btn btn-primary">View Drug Formulations</a>
+                                    </div>
+                                    <div class="col-md-6">
+                                        <a href="#" class="btn btn-info">View API Interactions</a>
+                                    </div>
+                                </div>
+                                
+                                {{-- <button type="submit" class="btn btn-primary pl-4 pr-4 mt-2">Submit</button> --}}
+                            </form>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="modal fade custom-modal add-modal" id="delete">
+        <div class="modal-dialog modal-dialog-centered">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title">Delete API</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <div class="row">
+                        <div class="col-12">
+                            <p>Deleting this API will produce cascading delete actions of the following</p>
+                            <ul>
+                                <li>All api interactions</li>
+                                <li>All drug formulations containing this API</li>
+                                <li>The API itself</li>
+                            </ul>
+                            <button class="btn btn-danger">Continue</button>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
 
 @push('scripts')
